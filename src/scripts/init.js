3afd44a6d1fd8b375b6dc675567e926b39f992da
Jun Shindo <jshindo@gladd.jp>
2017-04-07 00:20:21 +0900

    delete all

--- src/scripts/init.js
+++
@@ -1,9 +0,0 @@
-{
-  const historyData = cc.historyData.data;
-
-  // データがあったら前回の状態を復元
-  if (historyData.length) {
-    cc.form.restoreValues(historyData[0]);
-    pubsub.pub('change.historyData', historyData);
-  }
-}
